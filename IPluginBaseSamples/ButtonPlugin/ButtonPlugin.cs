152b3be35d25cf8ee4c3de9a90af27b936b5a7dd
Rasmus Aaberg Hansen <rh@sit.dk>
2018-04-25 13:56:24 +0200

    Added IPluginBase

---
+++ IPluginBaseSamples/ButtonPlugin/ButtonPlugin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Uniconta.API.Plugin;
+using Uniconta.API.Service;
+using Uniconta.ClientTools.DataModel;
+using Uniconta.Common;
+
+namespace ButtonPlugin
+{
+    public class ButtonPlugin : IPluginBase
+    {
+        // Fields
+        private string errorDescription;
+
+        public string Name { get { return "ButtonPlugin"; } }
+
+        public void Intialize()
+        {
+            errorDescription = "No Error Set";
+        }
+
+        public ErrorCodes Execute(UnicontaBaseEntity master, UnicontaBaseEntity currentRow, IEnumerable<UnicontaBaseEntity> source, string command, string args)
+        {
+            var debtor = currentRow as DebtorClient;
+            if(debtor == null)
+            {
+                errorDescription = "No row or wrong type selected";
+                return ErrorCodes.Exception;
+            }
+
+            MessageBox.Show($"Selected Debtor: {debtor.Account}");
+
+            return ErrorCodes.Succes;
+        }
+
+        public string[] GetDependentAssembliesName()
+        {
+            return new string[] { };
+        }
+
+        public string GetErrorDescription()
+        {
+            return errorDescription;
+        }
+
+
+        public void SetAPI(BaseAPI api)
+        {
+        }
+
+        public void SetMaster(List<UnicontaBaseEntity> masters)
+        {
+        }
+
+        public event EventHandler OnExecute;
+    }
+}
---
+++ IPluginBaseSamples/PrePostEventPlugin/PrePostEventPlugin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Uniconta.API.Plugin;
+using Uniconta.API.Service;
+using Uniconta.Common;
+
+namespace PrePostEventPlugin
+{
+    public class PrePostEventPlugin : IPluginBase
+    {
+        // Fields
+        private string errorDescription;
+
+        public string Name { get { return "PrePostEventPlugin"; } }
+
+        public void Intialize()
+        {
+            errorDescription = "No Error Set";
+        }
+
+        public ErrorCodes Execute(UnicontaBaseEntity master, UnicontaBaseEntity currentRow, IEnumerable<UnicontaBaseEntity> source, string command, string args)
+        {
+            MessageBox.Show($"Command: {command}");
+            return ErrorCodes.Succes;
+        }
+
+        public string[] GetDependentAssembliesName()
+        {
+            return new string[] { };
+        }
+
+        public string GetErrorDescription()
+        {
+            return errorDescription;
+        }
+
+
+        public void SetAPI(BaseAPI api)
+        {
+        }
+
+        public void SetMaster(List<UnicontaBaseEntity> masters)
+        {
+        }
+
+        public event EventHandler OnExecute;
+    }
+}
